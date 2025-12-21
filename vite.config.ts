98d47d8f59a7e895ae48829158ef5711e3a81d10
Cor Zuurmond <jczuurmond@pm.me>
2025-12-21 21:17:33 +0100

    Revert "Set subpage"
    This reverts commit 55564134630dfb6c2c447bf90197f003b2523fd8.

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
-  base: '/heleen-s-digital-twin/',
+export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
